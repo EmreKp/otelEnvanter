0282e162e6d22d29938a20c959959005ac4e5b04
Emre Kaplan <emrekp@outlook.com>
2017-09-26 00:53:09 +0300

    Update code

--- src/app/Http/Controllers/AnaKontroler.php
+++ src/app/Http/Controllers/AnaKontroler.php
@@ -7,7 +7,7 @@ use DB;
 
 class AnaKontroler extends Controller
 {
-    public function index(){
+	public function index(){
 		$req=Request::input();
 		if(!Request::has("checkIn") || !Request::has("checkOut") || !Request::has("pax")){
 			return response("Hata: Parametrelerin hepsini girmeniz gerekmektedir.",400);
@@ -15,40 +15,30 @@ class AnaKontroler extends Controller
 		$checkIn=$req["checkIn"];
 		$checkOut=$req["checkOut"];
 		$pax=$req["pax"];
-		$dblOda=DB::table("inventories")->where([
-			["room_code","=","STDDBL"],
+		$odaAl=DB::table("inventories")->where([
 			["date",">=",$checkIn],
 			["date","<",$checkOut]
 		])->get();
-		$snglOda=DB::table("inventories")->where([
-			["room_code","=","STDSNGL"],
-			["date",">=",$checkIn],
-			["date","<",$checkOut]
-		])->get();
-		$dblTotal=0; $snglTotal=0;
-		foreach($dblOda as $dbl){
-			if($dbl->allotment==0 || $dbl->max_pax<$pax){
-				$dblTotal=null;
-				break;
+		$odalar=$odaAl->groupBy("room_code");
+		$keys=$odalar->keys();
+		$prices=[];
+		foreach($keys as $k){
+			$fyt=[];
+			$total=0;
+			foreach($odalar[$k] as $oda){
+				if ($oda->allotment==0 || $oda->max_pax<$pax){
+					$total=null;
+					break;
+				}
+				$fiyat=$oda->price;
+				$indir=$oda->discount;
+				if($indir==null) $indir=0;
+				$fiyat=$fiyat-$fiyat*$indir/100;
+				$total+=$fiyat;
 			}
-			$fiyat=$dbl->price;
-			$indir=$dbl->discount;
-			if($indir==null) $indir=0;
-			$dblTotal+=$fiyat-$fiyat*$indir/100;
+			$fyt=["roomCode" => $k, "price" => $total];
+			$prices[]=$fyt;
 		}
-		foreach ($snglOda as $sngl){
-			if($sngl->allotment==0 || $sngl->max_pax<$pax){
-				$snglTotal=null;
-				break;
-			}
-			$fiyat=$sngl->price;
-			$indir=$sngl->discount;
-			if($indir==null) $indir=0;
-			$snglTotal+=$fiyat-$fiyat*$indir/100;
-		}
-		return response()->json([
-			["roomCode" => "STDSNGL", "price" => $dblTotal],
-			["roomCode" => "STDDBL", "price" => $snglTotal]
-		]);
+		return response()->json($prices);
 	}
 }
